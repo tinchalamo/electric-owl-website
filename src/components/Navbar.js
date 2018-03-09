475c788afb6fde46cfab43146a7328ebef404fd5
Jason Hummel <jason@madebychalk.com>
2018-03-09 15:39:19 -0700

    updates header with company name and contact button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,12 +12,19 @@ const Navbar = () => (
           <figure className="image">
             <img src={logo} alt="Electric Owl Creative" style={{ width: '88px' }} />
           </figure>
+          Electric Owl Creative
         </Link>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
+      <div className="navbar-end">
+        <div class="navbar-item">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+        </div>
+
+        <div class="navbar-item">
+          <a class="button is-info" href="mailto:info@electricowlcreative.com">Contact</a>
+        </div>
       </div>
     </div>
   </nav>
